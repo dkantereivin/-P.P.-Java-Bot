13440a0e3016452c9abc68d9661762a9a6d2234a
Alphasam123 <Alphasam123@gmail.com>
2020-05-22 12:45:21 +0100

    Create SnippetCommand.java

---
+++ src/main/java/snippets/SnippetCommand.java
@@ -0,0 +1,19 @@
+package snippets;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+public class SnippetCommand extends Command {
+	
+	public SnippetCommand() {
+		this.name = "snippet";
+		this.help = "find pre-written code";
+	}
+
+	@Override
+	protected void execute(CommandEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
